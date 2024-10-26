4d5ad06a0c82fa71c7c87cdb025b7963420e382d
Hacker-From-Fallout <chernyshovignat87@gmail.com>
2024-10-26 14:53:17 +0300

    Полуживая авторизация

--- manager-app/src/main/java/chernyshov/ignat/manager/ManagerApplication.java
+++ manager-app/src/main/java/chernyshov/ignat/manager/ManagerApplication.java
@@ -2,11 +2,15 @@ package chernyshov.ignat.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ManagerApplication {
 	
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class, args);
+        
+        System.out.println(new BCryptPasswordEncoder()
+        		.encode("password"));
     }
 }
