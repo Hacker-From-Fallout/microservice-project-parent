0f10dde1109df3650949c1b1c63ac8b102460849
Hacker-From-Fallout <chernyshovignat87@gmail.com>
2024-11-05 10:04:27 +0300

    admin-server

--- admin-server/src/main/java/chernyshov/ignat/admin/config/SecurityBeans.java
+++ admin-server/src/main/java/chernyshov/ignat/admin/config/SecurityBeans.java
@@ -1,6 +1,8 @@
 package chernyshov.ignat.admin.config;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 
 import chernyshov.ignat.admin.web.client.OAuthHttpHeadersProvider;
@@ -20,7 +29,7 @@ import jakarta.annotation.Priority;
 @Configuration
 public class SecurityBeans {
     
-    @Bean
+	@Bean
     public OAuthHttpHeadersProvider oAuthHttpHeadersProvider(
             ClientRegistrationRepository clientRegistrationRepository,
             OAuth2AuthorizedClientService authorizedClientService
@@ -50,7 +59,27 @@ public class SecurityBeans {
         return http
                 .oauth2Client(Customizer.withDefaults())
                 .oauth2Login(Customizer.withDefaults())
-                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().hasRole("ADMIN"))
                 .build();
     }
+    
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
+        OidcUserService oidcUserService = new OidcUserService();
+        return userRequest -> {
+            OidcUser oidcUser = oidcUserService.loadUser(userRequest);
+            List<GrantedAuthority> authorities =
+                    Stream.concat(oidcUser.getAuthorities().stream(),
+                            Optional.ofNullable(oidcUser.getClaimAsStringList("groups"))
+                                    .orElseGet(List::of)
+                                    .stream()
+                                    .filter(role -> role.startsWith("ROLE_"))
+                                    .map(SimpleGrantedAuthority::new)
+                                    .map(GrantedAuthority.class::cast))
+                            .toList();
+
+            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+        };
+    }
 } 
+
--- catalogue-service/src/main/java/chernyshov/ignat/catalogue/config/SecurityBeans.java
+++ catalogue-service/src/main/java/chernyshov/ignat/catalogue/config/SecurityBeans.java
@@ -11,16 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityBeans {
-
-//	@Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//            .csrf().disable() // Отключить CSRF
-//            .authorizeHttpRequests(authorize -> authorize
-//                .anyRequest().permitAll() // Разрешить доступ ко всем запросам
-//            );
-//        return http.build();
-//    }
 	
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
