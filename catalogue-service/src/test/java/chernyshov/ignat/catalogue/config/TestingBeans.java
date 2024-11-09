22e63b76f3cdc8db5f815e7b2603b9c4ca4486eb
Hacker-From-Fallout <chernyshovignat87@gmail.com>
2024-11-09 20:14:03 +0300

    loki logs

--- catalogue-service/src/test/java/chernyshov/ignat/catalogue/config/TestingBeans.java
+++ catalogue-service/src/test/java/chernyshov/ignat/catalogue/config/TestingBeans.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.mock;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @Configuration
@@ -11,6 +12,11 @@ public class TestingBeans {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return mock(JwtDecoder.class);
+        return mock();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return mock();
     }
 }
--- manager-app/src/test/java/chernyshov/ignat/manager/config/TestingBeans.java
+++ manager-app/src/test/java/chernyshov/ignat/manager/config/TestingBeans.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.client.RestClient;
 
 import chernyshov.ignat.manager.client.RestClientProductsRestClient;
@@ -18,12 +19,17 @@ public class TestingBeans {
 
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
-        return mock(ClientRegistrationRepository.class);
+        return mock();
     }
 
     @Bean
     public OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository() {
-        return mock(OAuth2AuthorizedClientRepository.class);
+        return mock();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return mock();
     }
 
     @Bean
